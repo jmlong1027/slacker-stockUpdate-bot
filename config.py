1c02639cc1d1dc733497dc273ab845366894d42a
Mike Long <michaellong1231@yahoo.com>
2022-01-05 17:15:10 -0500

    Add files via upload
    configuration file where you place slacker key and stock dict for main.py

---
+++ config.py
@@ -0,0 +1,9 @@
+slack_key = "ENTER SLACK KEY HERE"
+
+# a nested dictionary which tracks stocks we purchased various data on it
+# Ticker is the key
+##
+gwsic_ticker ={'NOW':{'name':'ServiceNow', 'pur_date':'08-01-2021', 'qty':1, 'pur_price':1.00, 'total':1.00},
+               'DT':{'name':'DynaTrace', 'pur_date':'07-27-2021', 'qty':2, 'pur_price':2.00, 'total':4.00},
+               'DIS':{'name':'Disney Walt Co Com', 'pur_date':'10-19-2021', 'qty':3, 'pur_price':3.00, 'total':9.00}
+               }
